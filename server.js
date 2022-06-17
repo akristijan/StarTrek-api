e702b6e68447de162a8a95d99862de9a667da3f2
Kristijan Aleksic <kristijan.aleksic@gmail.com>
2022-06-17 15:06:40 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 8005
+require('dotenv').config()
+
+let db,
+    dbConnectionStr = process.env.DB_STRING
+    dbName="star-trek-api"
+
+MongoClient.connect(dbConnectionStr)
+    .then(client => {
+        console.log(`Connected to the ${dbName} Database`)
+        db = client.db(dbName)
+    })
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
