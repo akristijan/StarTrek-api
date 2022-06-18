6182e7ab802bdd00afd63cefc7d88f765e8c9b82
Kristijan Aleksic <kristijan.aleksic@gmail.com>
2022-06-18 15:28:17 +0200

    add first solution

---
+++ public/main.js
@@ -0,0 +1,48 @@
+document.querySelector('#updateBtn').addEventListener('click', updateEntry)
+document.querySelector('#deleteBtn').addEventListener('click', deleteEntry)
+
+
+
+async function updateEntry() {
+    try {
+        const response = await fetch('updateEntry', {
+            method: 'put',
+            headers : {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                name: document.getElementsByName('name')[0].value,
+                species:document.getElementsByName('species')[0].value,
+                features:document.getElementsByName('features')[0].value,
+                homeWorld:document.getElementsByName('homeworld')[0].value,
+                img:document.getElementsByName('image')[0].value,
+                interestingFact:document.getElementsByName('interestingFact')[0].value,
+                notableExamples:document.getElementsByName('notableExamples')[0].value,
+            })
+        })
+
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }
+    catch(err) {
+        console.log(err)
+    }
+}
+
+async function deleteEntry() {
+    const input = document.getElementById('deleteInput')
+
+    try {
+        const response = await fetch('deleteEntry', {
+            method: "delete",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                name: input.value
+            })
+        })
+        const data = await response.json()
+        location.reload()
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
+//REQUIRE DEPNEDENCIES
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 8005
 require('dotenv').config()
 
+//DECLARED DB VARIABLES
 let db,
-    dbConnectionStr = process.env.DB_STRING
+    dbConnectionStr = process.env.DB_STRING,
     dbName="star-trek-api"
 
 MongoClient.connect(dbConnectionStr)
@@ -14,6 +16,80 @@ MongoClient.connect(dbConnectionStr)
         db = client.db(dbName)
     })
 
+
+
+//SET Middleware
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended : true}))
+app.use(express.json())
+
+//CRUD METHODS
+app.get('/', (req, res) => {
+    db.collection('aliens').find().toArray()
+        .then(data => {
+            let nameList = data.map(item => item.name)
+            console.log(nameList)
+            res.render('index.ejs', {info : nameList})
+        })
+        .catch(error => console.log(error))
+    
+    })
+
+app.post('/api', (req, res) => {
+    const body = req.body
+
+    db.collection('aliens').insertOne(
+        {
+            name : body.name,
+            species: body.species,
+            features: body.features,
+            homeWorld: body.homeWorld,
+            img : body.image,
+            interestingFact: body.interestingFact,
+            notableExamples: body.notableExamples,
+        }
+    )
+    .then(result => {
+        console.log(result)
+        res.redirect('/')
+    })
+})
+
+app.put('/updateEntry', (req, res) => {
+    console.log(req.body)
+    Object.keys(req.body).forEach(key => {
+        if(req.body[key] === null || req.body[key] === undefined || req.body[key] === '') {
+            delete req.body[key]
+        }
+
+    })
+    console.log(req.body)
+    db.collection('aliens').findOneAndUpdate(
+        {name : req.body.name},
+        {
+            $set: req.body
+        }
+    )
+    .then(result => {
+        console.log(result)
+        res.json('Success')
+    })
+    .catch(error => console.log(error))
+}) 
+
+app.delete('/deleteEntry', (req, res) => {
+    db.collection('aliens').findOneAndDelete(
+        {name: req.body.name}
+        )
+    .then(result => {
+        console.log("Entry deleted!")
+        res.json("Entry deleted")
+    })
+    .catch(error => console.log(error))
+})
+
+//SET UP LOCALHOST ON PORT
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
