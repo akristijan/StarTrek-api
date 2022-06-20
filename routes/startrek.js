6e1edda268bc1fa3c64555486da4abe6df970c42
Kristijan Aleksic <kristijan.aleksic@gmail.com>
2022-06-20 15:00:40 +0200

    Added get alien by id

--- routes/startrek.js
+++ routes/startrek.js
@@ -8,10 +8,7 @@ const Alien = require('../models/alien')
 router.get('/', (req, res) => {
     Alien.find().sort({createdAt: -1})
         .then(data => {
-            let listAliens = data.map(alien => alien)
-            
-            console.log(listAliens)
-            res.render('index.ejs', {info : listAliens})
+            res.render('index.ejs', {info : data})
         })
         .catch(error => console.log(error))
     
@@ -19,10 +16,10 @@ router.get('/', (req, res) => {
 
 router.get('/aliens/:id', (req, res) => {
     const id = req.params.id;
-    console.log(id)
+    
     Alien.findById(id)
         .then(result => {
-            res.render('details', {alien : result, title: "Alien details"})
+            res.render('details', {alien : result, title: "Alien details"});
         })
         .catch(error => console.log(error))
 
@@ -39,36 +36,11 @@ router.post('/api', (req, res) => {
     })
 
 router.put('/updateEntry', (req, res) => {
-        console.log(req.body)
-        Object.keys(req.body).forEach(key => {
-            if(req.body[key] === null || req.body[key] === undefined || req.body[key] === '') {
-                delete req.body[key]
-            }
-    
-        })
-        console.log(req.body)
-        db.collection('aliens').findOneAndUpdate(
-            {name : req.body.name},
-            {
-                $set: req.body
-            }
-        )
-        .then(result => {
-            console.log(result)
-            res.json('Success')
-        })
-        .catch(error => console.log(error))
+        
     }) 
     
 router.delete('/deleteEntry', (req, res) => {
-        db.collection('aliens').findOneAndDelete(
-            {name: req.body.name}
-            )
-        .then(result => {
-            console.log("Entry deleted!")
-            res.json("Entry deleted")
-        })
-        .catch(error => console.log(error))
+        
     })
 
 module.exports = router
